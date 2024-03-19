815d815901edd3b3a94c0f43d3df06d007063c7e
Vinicius Rebelatto <vinicius-rebelatto@hotmail.com>
2024-03-18 23:24:58 -0300

    index.js changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    return res.json({message: 'Server is upgit init'})
+    return res.json({message: 'Server is up'})
 })
 
 app.listen(3333)
